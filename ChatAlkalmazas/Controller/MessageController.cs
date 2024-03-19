1f58b8b0437ecee814b379da843a524082468f60
Zsombor <csabazsombi@stud.uni-obuda.hu>
2024-03-19 11:33:32 +0100

    Controllers done

--- ChatAlkalmazas/Controller/MessageController.cs
+++ ChatAlkalmazas/Controller/MessageController.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using ChatAlkalmazas.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Models;
+using System.Collections.Generic;
+
+
+namespace ChatAlkalmazas.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+
+        IHubContext<SignalRHub> hub;
+
+        private static List<Message> messages = new List<Message>();
+
+        public MessageController(IHubContext<SignalRHub> hub)
+        {
+            this.hub = hub;
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Message value)
+        {
+            messages.Add(value);
+            this.hub.Clients.All.SendAsync("MessageCreated", value);
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> ReadAll()
+        {
+            return messages;
+        }
+    }
+}
