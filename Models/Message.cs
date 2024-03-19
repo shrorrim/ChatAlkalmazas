0aca7c4312a134db2d5963c1f64dd9a6afae96d6
Zsombor <csabazsombi@stud.uni-obuda.hu>
2024-03-19 11:16:17 +0100

    Models done

---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class Message
+    {
+        public string Sender { get; set; }
+
+        public DateTime SendDate { get; set; } = DateTime.Now;
+
+        public string SentMessage { get; set; }
+    }
+}
