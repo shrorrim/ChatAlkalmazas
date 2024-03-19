64520e4417487a6d50ed41ed8a8235148749375b
Marcell Nagy <nagymarcell2@hotmail.com>
2024-03-19 11:29:16 +0100

    references

--- ChatAlkalmazasGUI/MainWindowViewModel.cs
+++ ChatAlkalmazasGUI/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace ChatAlkalmazasGUI
 {
     public class MainWindowViewModel 
     {
+
+        public RestCollection<Message> Messages { get; set; }
+
+
+
+        public MainWindowViewModel()
+        {
+            //Messages = new RestCollection<Message>();
+        }
+
     }
 }
